600aa2380fb684817c531954ad2b481922affb3b
dujun <jun.du@intel.com>
2023-01-10 04:41:20 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     keywords='pytorch pretrained models efficientnet mobilenetv3 mnasnet',
     packages=find_packages(exclude=['convert', 'tests', 'results']),
     include_package_data=True,
-    install_requires=['torchvision'],
+    install_requires=[],
     python_requires='>=3.6',
 )
